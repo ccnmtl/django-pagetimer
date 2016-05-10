9c2028d067e930e5e717345a6abc73db803fd3af
Anders Pearson <anders@columbia.edu>
2016-05-10 13:29:48 +0100

    expose interval on dashboard

--- pagetimer/views.py
+++ pagetimer/views.py
@@ -1,5 +1,6 @@
 import csv
 
+from django.conf import settings
 from django.http import HttpResponse, JsonResponse
 from django.views.generic.base import View
 from django.views.generic.list import ListView
@@ -27,10 +28,21 @@ class PageTimerEndpointView(View):
         return JsonResponse({"status": "ok"})
 
 
+def get_interval():
+    if hasattr(settings, 'PAGETIMER_INTERVAL'):
+        return settings.PAGETIMER_INTERVAL
+    return 60
+
+
 # TODO: superuser-only
 class DashboardView(ListView):
     template_name = "pagetimer/dashboard.html"
     model = PageVisit
+
+    def get_context_data(self, **kwargs):
+        context = super(DashboardView, self).get_context_data(**kwargs)
+        context['interval'] = get_interval()
+        return context
 
 
 # TODO: superuser-only
