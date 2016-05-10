cb0d254cc02f6c9bfd8663e5fdd9b3a61dafedca
Anders Pearson <anders@columbia.edu>
2016-05-10 11:14:17 +0100

    logged in test

--- pagetimer/tests/test_views.py
+++ pagetimer/tests/test_views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
 from django.test import TestCase
 from pagetimer.models import PageVisit
@@ -12,4 +13,18 @@ class SimpleTest(TestCase):
         self.assertEqual(PageVisit.objects.count(), 1)
         v = PageVisit.objects.all()[0]
         self.assertEqual(v.username, 'anonymous')
+        self.assertEqual(v.path, '/something/random')
+
+    def test_logged_in(self):
+        u = User.objects.create(username='testuser')
+        u.set_password('password')
+        u.save()
+        self.client.login(username='testuser', password='password')
+        r = self.client.post(
+            reverse('pagetimer-endpoint'),
+            dict(path='/something/random'))
+        self.assertEqual(r.status_code, 200)
+        self.assertEqual(PageVisit.objects.count(), 1)
+        v = PageVisit.objects.all()[0]
+        self.assertEqual(v.username, 'testuser')
         self.assertEqual(v.path, '/something/random')
